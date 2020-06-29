20d3107117c77f34c7f5fcc32120eef7ca800d54
seancabahug <25161923+seancabahug@users.noreply.github.com>
2020-06-28 18:45:31 -0700

    procfile

---
+++ frontend/build/precache-manifest.d053d39897a476856888b52e1ff15fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47c1e5f29032601701eedf2388fd25e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4c37e9c0c1af9394d65",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "73c0b8646dae99b16c35",
+    "url": "/static/css/main.85e6ed75.chunk.css"
+  },
+  {
+    "revision": "c4c37e9c0c1af9394d65",
+    "url": "/static/js/2.6163aa8d.chunk.js"
+  },
+  {
+    "revision": "b9c4eabe83d6f0e9b4a297941298ecc1",
+    "url": "/static/js/2.6163aa8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c0b8646dae99b16c35",
+    "url": "/static/js/main.9c4478c4.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
